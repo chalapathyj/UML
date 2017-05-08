40276b86d0209a52d4890fd6b135af7e535cd421
chalapathyj <chala_shank@yahoo.co.in>
2017-05-08 18:43:02 +0530

    Update Base_class.py

--- Base_class.py
+++ Base_class.py
@@ -1,6 +1,6 @@
-from Inventory import Notice, SrClerk
+from Inventory import SrClerk
 #notice = Notice('Abn1', 'corp1', 'book', '5')
-srclrk = SrClerk("3", "ram")
+srclrk = SrClerk("3", "ram", '1024','acb shipng', 'Books', '5')
 
 #notice.generateNotice('srclerk', 'Ordered book. But pens are supplied.')
-srclrk.generateErrorNotice('Ordered book. But pens are delivered.')
+srclrk.verifyShipment()
