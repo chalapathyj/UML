6883c12c51e0f2e85b9dfc532459c7125ca4f235
chalapathyj <chala_shank@yahoo.co.in>
2017-05-09 04:10:54 +0530

    Update Base_class.py

--- Base_class.py
+++ Base_class.py
@@ -1,6 +1,13 @@
-from Inventory import SrClerk
-#notice = Notice('Abn1', 'corp1', 'book', '5')
-srclrk = SrClerk("3", "ram", '1024','acb shipng', 'Books', '5')
+from Inventory import SrClerk, InventoryItem
+
+# InventoryItem.items = { 'id' : 001 , 'cat' : 'fruits', 'name':'apple', 'actqua' : 10, 'ecordqua' : 20
+# }
+# #notice = Notice('Abn1', 'corp1', 'book', '5')
+invnt = InventoryItem('003','fancy', 'cap', '5', '7')
+invnt.getQuantity('001')
+
+srclrk = SrClerk("3", "ram", '1024', 'acb shipng', 'Books', '5')
+
 
 #notice.generateNotice('srclerk', 'Ordered book. But pens are supplied.')
-srclrk.verifyShipment()
+#srclrk.verifyShipment()
